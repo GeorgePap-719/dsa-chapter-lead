b602cd65a73b29b1d76fb2ddd7c15a1b33109e95
George Papadopoulos <george.719pap@gmail.com>
2025-04-14 19:58:10 +0300

    Add ImmutableString

---
+++ src/main/kotlin/ImmutableString.kt
@@ -0,0 +1,11 @@
+package org.example
+
+//TODO
+class ImmutableString {
+//    operator fun plus(other: Any?): String {}
+//    val length
+//fun get(index: Int): Char
+//fun subSequence(standIndex: Int, endIndex: Int): CharSequence
+//    override fun equals(other: Any?): Boolean {}
+//    override fun toString(): String {}
+}
